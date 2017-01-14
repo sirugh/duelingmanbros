200c1c8bece8897456a454d229e9bf3c93880357
Stephen Rugh <sirugh@users.noreply.github.com>
2017-01-13 23:18:58 -0600

    Adding keys and new song logic (#3)

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -1,3 +1,5 @@
+const NOTES = ['d', 'f', 'a', 'c', 'e', 'g']
+
 // =======================================
 // Create the AirConsole instance
 // =======================================
@@ -5,17 +7,32 @@ const airconsole = new AirConsole({
   orientation: AirConsole.ORIENTATION_LANDSCAPE
 });
 
-const role_info_ele = $('#role_info')
-
-
-airconsole.onReady = function() {};
-
+// Notes is an array that correlates to the different notes on the staff.
+let notes = []
+const $staff = $('#staff')
 airconsole.onMessage = function(device_id, data) {
   // A mapping of action-to-behaviors.
   const actions = {
     RESET_SONG: function (data) {
-      $('#song_title').html(`SONG: "${data.song_name}"`)
-      navigator.vibrate(1000);
+      $('#song_title').html(`SONG: "${data.song}"`)
+      notes = new Array(data.numNotes)
+      notes[0] = data.startingNote
+
+      // Generate the new staff top-down, left-right.
+      $staff.empty()
+      for (let i = 5; i >= 0; i--) {
+        let $row = $(`<tr id="row_${NOTES[i]}"></tr>`)
+        for (let j = 0; j <= notes.length; j++) {
+          $row.append(
+            `<td class="note">
+              <button id="${NOTES[i]}-${j}">${NOTES[i]}</button>
+            </td>`)
+        }
+        $staff.append($row)
+      }
+
+      // TODO: this should work outside the sim.
+      // navigator.vibrate(1000);
     }
   }
 
@@ -38,4 +55,12 @@ $('#submit').on('click', function() {
     notes: ['c', 'b', 'g', 'b#']
   });
 });
+
+$('.note').on('click', function () {
+  // TODO: Set column based on where this was clicked in the table.
+  const column = 0
+
+  // TODO: Set based on actual note value
+  notes[column] = 'a'
+})
 
