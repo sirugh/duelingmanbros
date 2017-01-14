ee7bbce350f4382b22bf8298edd60f014e062b8a
Stephen Rugh <stephenrugh@gmail.com>
2017-01-13 23:18:29 -0600

    Adding keys and new song logic

--- js/screen/game.js
+++ js/screen/game.js
@@ -15,14 +15,22 @@ const game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', {
   render: render
 });
 
+const STATE_STARTED = 'STARTED'
+const STATE_WAITING = 'WAITING'
+const STATE_END     = 'END'
+
+let currentState = STATE_WAITING
+
 const players = [
   {
-    score: 0
+    score: 0,
+    notes: []
   },
   {
-    score: 0
+    score: 0,
+    notes: []
   }
-];
+]
 
 function preload () {
     // game.load.image('logo', 'assets/logo.png');
@@ -35,7 +43,7 @@ function preload () {
 }
 
 function create () {
-  const  airconsole = new AirConsole();
+  const airconsole = new AirConsole();
 
   // As soon as a device connects we add it to our device-map
   airconsole.onConnect = function(device_id) {
@@ -62,29 +70,19 @@ function create () {
       players[player].notes = data.notes;
     }
 
-    //
-    if (players[0].notes && players[1].notes) {
+    if (players[0].notes.length && players[1].notes.length) {
       console.log('Both players submitted!');
       // Do score calculations.
-      // Display score changes.
+
       // Perform animations: http://phaser.io/examples/v2/animation/animation-events
       players.forEach(player => {
-        sendNewSong()
+
       })
+      sendNewSong()
     }
   };
 
   game.camera.focusOnXY(0, 0);
-
-  function sendNewSong(player) {
-    airconsole.message(
-      airconsole.convertPlayerNumberToDeviceId(player),
-      {
-        action: 'RESET_SONG',
-        song: 'Get Schwifty',
-        numNotes: 12// the number of notes that this song contains
-      })
-  }
 
   /**
    * Checks if two players are connected!
@@ -99,6 +97,8 @@ function create () {
         // Setting the first 2 controllers to active players.
         airconsole.setActivePlayers(2);
         document.getElementById("wait").innerHTML = "";
+        // Once we get two players, set the current state to "STARTED"
+        startGame()
       } else if (connected_controllers.length == 1) {
         document.getElementById("wait").innerHTML = "Need 1 more player!";
       } else if (connected_controllers.length == 0) {
@@ -106,12 +106,47 @@ function create () {
       }
     }
   }
+
+  function startGame() {
+    currentState = STATE_STARTED
+
+    players.forEach(player => {
+      player.score = 0
+    })
+
+    // Emit the first song.
+    sendNewSong()
+  }
+
+  function sendNewSong() {
+    players.forEach(player => {
+      player.notes = []
+    })
+    for (var i = 1; i <= 2; i++) {
+      airconsole.message(
+        airconsole.convertPlayerNumberToDeviceId(i),
+        {
+          action: 'RESET_SONG',
+          song: 'Dueling Banjos',
+          numNotes: 12,// the number of notes that this song contains
+          startingNote: 'g'
+        })
+    }
+  }
 }
 
 function update () {
-  // if(players[0].input && players[1].input) {
-  //   console.log('Updating after both players submitted input!')
-  // }
+  if (currentState === STATE_WAITING) {
+
+  }
+
+  if (currentState === STATE_STARTED) {
+
+  }
+
+  if (currentState === STATE_END) {
+
+  }
 }
 
 function render () {
